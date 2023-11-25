9db3ebb099e0dc4678ecb0fbc1a55bb03a584677
Halit MUBAYYİD <32683018+halitmubayid@users.noreply.github.com>
2023-11-26 01:16:45 +0300

    26/11/2023

--- IstanbulLMC/Controllers/HomeController.cs
+++ IstanbulLMC/Controllers/HomeController.cs
@@ -16,34 +16,12 @@ namespace IstanbulLMC.Controllers
         }
         
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            //string apiKey = "AIzaSyBM3yphMQm5oDBRuAinvFILcsWI5GYD19w";
-            //string baseUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json";
-
-            //string location = ""; // اسم المدينة أو الإحداثيات الجغرافية للموقع المراد البحث عن الأماكن فيه
-            //string query = "elit world"; // نوع الأماكن المطلوب البحث عنها
-
-            //string url = $"{baseUrl}?query={query}&location={location}&key={apiKey}";
-
-            //using (HttpClient client = new HttpClient())
-            //{
-            //    HttpResponseMessage response = await client.GetAsync(url);
-
-            //     if (response.IsSuccessStatusCode)
-            //    {
-            //        string jsonResponse = await response.Content.ReadAsStringAsync();
-            //    }
-            //    else
-            //    {
-
-            //    }
-            //}
-
             return View();
         }
-        [HttpGet]
 
+        [HttpGet]
         public async Task<IActionResult> GetPlaceByName(string name)
         {
             string apiKey = "AIzaSyBM3yphMQm5oDBRuAinvFILcsWI5GYD19w";
@@ -71,6 +49,33 @@ namespace IstanbulLMC.Controllers
             return View("Error"); // Return an error view or appropriate response
         }
 
+        [HttpGet]
+        public async Task<IActionResult> VehicleList(TransferDTO transferDTO)
+        {
+            string apiKey = "AIzaSyBM3yphMQm5oDBRuAinvFILcsWI5GYD19w";
+            string baseUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json";
+
+            string location = ""; // اسم المدينة أو الإحداثيات الجغرافية للموقع المراد البحث عن الأماكن فيه
+            string query = name; // نوع الأماكن المطلوب البحث عنها
+
+            string url = $"{baseUrl}?query={query}&location={location}&key={apiKey}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    var jsonResponse = JsonConvert.DeserializeObject<ApiResponse>(responseContent);
+
+                    var topFivePlaces = jsonResponse.subApiResponses.Take(5).ToList();
+                    return PartialView("_SearchResultsPartial", topFivePlaces);
+
+                }
+            }
+            return View("Error"); // Return an error view or appropriate response
+        }
 
         public IActionResult Privacy()
         {
---
+++ IstanbulLMC/DTOs/TransferDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace IstanbulLMC.DTOs
+{
+    public class TransferDTO
+    {
+        public int VehicleCategoryID { get; set; }
+
+        public string? FromPlaceID { get; set; }
+
+        public string? ToPlaceID { get; set; }
+
+        public decimal? KMPrice { get; set; }
+
+        public decimal? TotalPrice { get; set; }
+
+        public decimal? Distance { get; set; }
+    }
+}
